68a63288f156a0fee96b551425b084ede7273dfd
Pavel <triodjangopiter@yandex.ru>
2017-08-18 14:47:03 +0300

    add spring mvc configuration

--- src/main/java/ru/pravvich/web/SpringMvcConfig.java
+++ src/main/java/ru/pravvich/web/SpringMvcConfig.java
@@ -1,12 +1,21 @@
 package ru.pravvich.web;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 /**
  * Author : Pavel Ravvich.
  * Created : 18.08.17.
- * <p>
- * SpringMvcConfig
  */
-public class SpringMvcConfig {
+@Configuration
+public class SpringMvcConfig extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
 
+        registry.addViewController("/get_all_items").setViewName("all_items");
+
+        registry.addViewController("/login").setViewName("login");
+    }
 }
