b79feea94d782d30c7e6ea7290c607da4da19fa1
Pavel <triodjangopiter@yandex.ru>
2017-08-18 14:47:46 +0300

    add spring secutity config

--- src/main/java/ru/pravvich/web/SpringSecurityConfig.java
+++ src/main/java/ru/pravvich/web/SpringSecurityConfig.java
@@ -1,7 +1,12 @@
 package ru.pravvich.web;
 
+import com.sun.tools.doclets.internal.toolkit.builders.AbstractMemberBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Author : Pavel Ravvich.
@@ -9,7 +14,34 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
  */
 @Configuration
 @EnableWebSecurity
-public class SprinSecurityConfig {
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
+        http
+                .authorizeRequests()
+
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/get_all_items")
+                .permitAll()
+
+                .and()
+
+                .logout().permitAll();
+    }
+
+    @Autowired
+    private void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("1").password("1").roles("USER");
+    }
 }
