17fd9a66d5ce1949c097a931cafc22497b487471
Pavel <triodjangopiter@yandex.ru>
2017-08-18 13:31:28 +0300

    add start app class for boot

--- src/main/java/ru/pravvich/Application.java
+++ src/main/java/ru/pravvich/Application.java
@@ -1,12 +1,15 @@
 package ru.pravvich;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Author : Pavel Ravvich.
  * Created : 18.08.17.
- * <p>
- * Application
  */
+@SpringBootApplication
 public class Application {
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
