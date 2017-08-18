61aea601d8302fa7de4ac4931e6f586ad094b015
Pavel <triodjangopiter@yandex.ru>
2017-08-18 14:44:45 +0300

    add sql schema empty template

---
+++ src/main/java/ru/pravvich/web/SpringSecurityConfig.java
@@ -0,0 +1,15 @@
+package ru.pravvich.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * Author : Pavel Ravvich.
+ * Created : 18.08.17.
+ */
+@Configuration
+@EnableWebSecurity
+public class SprinSecurityConfig {
+
+
+}
