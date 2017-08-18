61f88e830eca7c47b7fd0fb098a7cace30f8f801
Pavel <triodjangopiter@yandex.ru>
2017-08-18 14:56:16 +0300

    add items schema

--- sql/create.sql
+++ sql/create.sql
@@ -1,14 +1,21 @@
 -- CREATE DATABASE spring_boot;
 
 CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY ,
-  username VARCHAR(20) NOT NULL ,
-  password VARCHAR(20) NOT NULL ,
-  role INTEGER NOT NULL ,
+  id       SERIAL PRIMARY KEY,
+  username VARCHAR(20) NOT NULL,
+  password VARCHAR(20) NOT NULL,
+  role     INTEGER     NOT NULL,
   FOREIGN KEY (role) REFERENCES user_role (id)
 );
 
 CREATE TABLE IF NOT EXISTS user_role (
-  id SERIAL PRIMARY KEY ,
+  id   SERIAL PRIMARY KEY,
   role VARCHAR(10)
+);
+
+CREATE TABLE IF NOT EXISTS items (
+  id      SERIAL PRIMARY KEY,
+  name    TEXT    NOT NULL,
+  authtor INTEGER NOT NULL,
+  FOREIGN KEY (authtor) REFERENCES users (id)
 );
