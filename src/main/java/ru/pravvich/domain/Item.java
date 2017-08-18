b25d529425b483dcc549124c87f0d60d8f489db3
Pavel <triodjangopiter@yandex.ru>
2017-08-18 13:32:02 +0300

    add domain Item

--- src/main/java/ru/pravvich/domain/Item.java
+++ src/main/java/ru/pravvich/domain/Item.java
@@ -1,12 +1,64 @@
 package ru.pravvich.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Author : Pavel Ravvich.
  * Created : 18.08.17.
- * <p>
- * Item
  */
+@Entity
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
+    private String name;
 
+    public Item() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Item)) return false;
+
+        Item item = (Item) o;
+
+        if (id != item.id) return false;
+        return name.equals(item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
