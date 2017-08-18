6cbc1bfc87100a22cb65ceaa41ea11fd5f3e426f
Pavel <triodjangopiter@yandex.ru>
2017-08-18 13:33:48 +0300

    add Item service impl

--- src/main/java/ru/pravvich/service/ItemServiceImpl.java
+++ src/main/java/ru/pravvich/service/ItemServiceImpl.java
@@ -1,12 +1,34 @@
 package ru.pravvich.service;
 
+import org.assertj.core.util.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.pravvich.domain.Item;
+import ru.pravvich.repository.ItemRepository;
+
+import java.util.List;
+
 /**
  * Author : Pavel Ravvich.
  * Created : 18.08.17.
- * <p>
- * ItemServiceImpl
  */
-public class ItemServiceImpl {
+@Service
+public class ItemServiceImpl implements ItemService {
 
+    private final ItemRepository repository;
 
+    @Autowired
+    public ItemServiceImpl(ItemRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void add(Item item) {
+        repository.save(item);
+    }
+
+    @Override
+    public List<Item> getAll() {
+        return Lists.newArrayList(repository.findAll());
+    }
 }
