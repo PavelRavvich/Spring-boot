0e4954a75598cbd09dcba177d353b9e1df2dd671
Pavel <triodjangopiter@yandex.ru>
2017-08-20 00:27:37 +0300

    added sql schema users

--- sql/create.sql
+++ sql/create.sql
@@ -1,17 +1,23 @@
 -- CREATE DATABASE spring_boot;
 
-CREATE TABLE IF NOT EXISTS users (
-  id       SERIAL PRIMARY KEY,
-  username VARCHAR(20) NOT NULL,
-  password VARCHAR(20) NOT NULL,
-  role     INTEGER     NOT NULL,
-  FOREIGN KEY (role) REFERENCES user_role (id)
-);
-
 CREATE TABLE IF NOT EXISTS user_role (
   id   SERIAL PRIMARY KEY,
   role VARCHAR(10)
 );
+
+INSERT INTO user_role (role) VALUES ('USER');
+INSERT INTO user_role (role) VALUES ('ADMIN');
+
+CREATE TABLE IF NOT EXISTS users (
+  id       SERIAL PRIMARY KEY,
+  username VARCHAR(20) UNIQUE NOT NULL,
+  password VARCHAR(20) UNIQUE NOT NULL,
+  role     INTEGER     NOT NULL,
+  FOREIGN KEY (role) REFERENCES user_role (id)
+);
+
+INSERT INTO users (username, password, role) VALUES ('u', 'u', '1');
+INSERT INTO users (username, password, role) VALUES ('a', 'a', '1');
 
 CREATE TABLE IF NOT EXISTS items (
   id      SERIAL PRIMARY KEY,
@@ -19,3 +25,7 @@ CREATE TABLE IF NOT EXISTS items (
   authtor INTEGER NOT NULL,
   FOREIGN KEY (authtor) REFERENCES users (id)
 );
+
+INSERT INTO items (name, authtor) VALUES ('description1', '1');
+INSERT INTO items (name, authtor) VALUES ('description2', '1');
+INSERT INTO items (name, authtor) VALUES ('description3', '1');
