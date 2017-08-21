048c624ff1d7d82e5349fa55dc42cbac1cfecf16
Pavel <triodjangopiter@yandex.ru>
2017-08-21 05:43:31 +0300

    add User service

--- src/main/java/ru/pravvich/service/UserService.java
+++ src/main/java/ru/pravvich/service/UserService.java
@@ -1,12 +1,44 @@
 package ru.pravvich.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import ru.pravvich.domain.Role;
+import ru.pravvich.domain.User;
+import ru.pravvich.repository.RoleRepository;
+import ru.pravvich.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Author : Pavel Ravvich.
  * Created : 20.08.17.
- * <p>
- * UserService
  */
-public class UserService {
+@Component
+public class UserService implements UserDetailsService {
 
+    @Autowired
+    private RoleRepository roleRepo;
 
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        final User user = userRepo.findByUsername(username);
+
+        final Role role = roleRepo.findOne(user.getRoleId());
+
+        final List<Role> authorities = new ArrayList<>();
+
+        authorities.add(role);
+
+        user.setAuthorities(authorities);
+
+        return user;
+    }
 }
