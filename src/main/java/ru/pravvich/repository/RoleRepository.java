b5f5849d7fbfaa95efdc2ff810942c5f88887198
Pavel <triodjangopiter@yandex.ru>
2017-08-21 05:42:31 +0300

    add Role repo

--- src/main/java/ru/pravvich/repository/RoleRepository.java
+++ src/main/java/ru/pravvich/repository/RoleRepository.java
@@ -1,4 +1,7 @@
 package ru.pravvich.repository;
 
-public interface RoleRepository {
+import org.springframework.data.repository.CrudRepository;
+import ru.pravvich.domain.Role;
+
+public interface RoleRepository extends CrudRepository<Role, Integer> {
 }
