cb349a289cc83000378a7e6f8c90cf7264df70db
Pavel <triodjangopiter@yandex.ru>
2017-08-21 05:41:41 +0300

    add Role domain

--- src/main/java/ru/pravvich/domain/Role.java
+++ src/main/java/ru/pravvich/domain/Role.java
@@ -1,12 +1,71 @@
 package ru.pravvich.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
 /**
  * Author : Pavel Ravvich.
  * Created : 20.08.17.
- * <p>
- * Role
  */
-public class Role {
+@Entity(name = "user_role")
+public class Role implements GrantedAuthority {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
+    @Column(name = "role")
+    private String role;
+
+    public Role() {
+    }
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String getAuthority() {
+        return role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+
+        Role role = (Role) o;
+
+        return id == role.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
