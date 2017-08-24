19974a412e3756cee5c4f2b4130a6480e4fb114f
Pavel <triodjangopiter@yandex.ru>
2017-08-24 22:46:24 +0300

    add annotation @Service

--- src/main/java/ru/pravvich/service/UserService.java
+++ src/main/java/ru/pravvich/service/UserService.java
@@ -4,20 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
-import ru.pravvich.domain.Role;
+import org.springframework.stereotype.Service;
 import ru.pravvich.domain.User;
-import ru.pravvich.repository.RoleRepository;
 import ru.pravvich.repository.UserRepository;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Author : Pavel Ravvich.
  * Created : 20.08.17.
  */
-@Component
+@Service
 public class UserService implements UserDetailsService {
 
     @Autowired
