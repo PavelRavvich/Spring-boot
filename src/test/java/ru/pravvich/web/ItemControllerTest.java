b9f88c0156abeb409b634c4c6a9442cdd287b370
Pavel <triodjangopiter@yandex.ru>
2017-08-28 22:42:04 +0300

    add test

--- src/test/java/ru/pravvich/web/ItemControllerTest.java
+++ src/test/java/ru/pravvich/web/ItemControllerTest.java
@@ -3,6 +3,8 @@ package ru.pravvich.web;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -11,20 +13,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.pravvich.domain.Item;
 import ru.pravvich.service.ItemService;
-import ru.pravvich.service.UserService;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-
 
 import java.util.ArrayList;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ItemController.class)
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class, SpringSecurityConfig.class })
 public class ItemControllerTest {
 
     @Autowired
@@ -37,17 +39,35 @@ public class ItemControllerTest {
     @WithMockUser(username = "user", roles = "USER")
     public void whenGetAllItemsThenControllerReturnAllItems() throws Exception {
         given(
-            itemService.getAll()
+                itemService.getAll()
         ).willReturn(
                 new ArrayList<Item>()
         );
 
         mvc.perform(
-            get("/get_all_items").accept(MediaType.TEXT_HTML)
+                get("/get_all_items").accept(MediaType.TEXT_HTML)
         ).andExpect(
                 status().isOk()
         ).andExpect(
                 view().name("all_items")
         );
     }
+
+    @Test
+    @WithMockUser(username = "user", roles = "USER")
+    public void whenPostThenAdd() throws Exception {
+
+        Item item = new Item();
+        item.setDescription("test");
+
+        mvc.perform(
+                post("/get_all_items/add_item_page/add_item")
+                        .param("description", "test")
+        ).andExpect(
+                status().is3xxRedirection()
+        );
+
+        verify(itemService, times(1)).add(item);
+    }
+
 }
