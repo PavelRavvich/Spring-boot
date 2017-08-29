aba2b0feea9ab4ee022a6d1a3645162acc04e3b4
Pavel <triodjangopiter@yandex.ru>
2017-08-29 04:37:48 +0300

    update User for security model

--- src/main/java/ru/pravvich/domain/User.java
+++ src/main/java/ru/pravvich/domain/User.java
@@ -97,7 +97,7 @@ public class User implements UserDetails {
     }
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
+    public List<Role> getAuthorities() {
         return authorities;
     }
 
