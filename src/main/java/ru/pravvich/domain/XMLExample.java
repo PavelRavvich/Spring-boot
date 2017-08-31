80b1aec0e16edb6c02fffc0996e32cb8665ebf0b
Pavel <triodjangopiter@yandex.ru>
2017-09-01 02:01:14 +0300

    add jackson dep

---
+++ src/main/java/ru/pravvich/domain/XMLExample.java
@@ -0,0 +1,12 @@
+package ru.pravvich.domain;
+
+/**
+ * Author : Pavel Ravvich.
+ * Created : 01.09.17.
+ * <p>
+ * XMLExample
+ */
+public class XMLExample {
+
+
+}
---
+++ src/main/java/ru/pravvich/web/RestControllerExample.java
@@ -0,0 +1,12 @@
+package ru.pravvich.web;
+
+/**
+ * Author : Pavel Ravvich.
+ * Created : 01.09.17.
+ * <p>
+ * RestControllerExample
+ */
+public class RestControllerExample {
+
+
+}
