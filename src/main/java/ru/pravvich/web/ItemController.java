3adb2fd575b18060ae253b1d8d368a8ea15ae6ea
Pavel <triodjangopiter@yandex.ru>
2017-09-01 02:04:17 +0300

    optimaze import

--- src/main/java/ru/pravvich/web/ItemController.java
+++ src/main/java/ru/pravvich/web/ItemController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ru.pravvich.domain.Item;
 import ru.pravvich.domain.MySpringUser;
-import ru.pravvich.domain.User;
 import ru.pravvich.service.ItemService;
 
 import java.security.Principal;
