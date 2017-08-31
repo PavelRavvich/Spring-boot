b5eda8c9baeb8545b32d07ca3fa090123b25fc27
Pavel <triodjangopiter@yandex.ru>
2017-09-01 02:02:37 +0300

    add test obj for rest API

--- src/main/java/ru/pravvich/domain/XMLExample.java
+++ src/main/java/ru/pravvich/domain/XMLExample.java
@@ -1,12 +1,31 @@
 package ru.pravvich.domain;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * Author : Pavel Ravvich.
  * Created : 01.09.17.
- * <p>
- * XMLExample
  */
+@JacksonXmlRootElement(localName = "XMLExample")
 public class XMLExample {
 
+    @JacksonXmlProperty(localName = "description")
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private String description;
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public XMLExample(String description) {
+        this.description = description;
+    }
 }
