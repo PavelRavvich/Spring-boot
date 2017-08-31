e15c063641afd49e7658679b627c98d0df2df5db
Pavel <triodjangopiter@yandex.ru>
2017-09-01 02:07:11 +0300

    example rest controller

--- src/main/java/ru/pravvich/web/RestControllerExample.java
+++ src/main/java/ru/pravvich/web/RestControllerExample.java
@@ -1,12 +1,23 @@
 package ru.pravvich.web;
 
+import org.assertj.core.util.Lists;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.pravvich.domain.XMLExample;
+
+import java.util.List;
+
 /**
  * Author : Pavel Ravvich.
  * Created : 01.09.17.
- * <p>
- * RestControllerExample
  */
+@RestController
+@RequestMapping("xml_ex")
 public class RestControllerExample {
 
-
+    @GetMapping("data")
+    public List<XMLExample> getAllArticles() {
+        return Lists.newArrayList(new XMLExample("test"));
+    }
 }
